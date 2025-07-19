e0c24ea5006efcf2a87231603f476b0492e395fe
j22-35 <j2235@matsue-ct.ac.jp>
2025-07-19 01:30:38 +0000

    修正(エラー残ってる)

--- config/passport.js
+++ config/passport.js
@@ -12,14 +12,14 @@ module.exports = function (app) {
   done(null, user.id);
 });
 
-passport.deserializeUser(async function (id, done) {
- console.log("deserializeUser");
- try {
- const user = await User.findById(id);
- done(null, user);
- } catch (error) {
- done(error, null);
- }
+passport.deserializeUser(function (id, done) {
+  console.log("deserializeUser");
+  try {
+    const user = User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
 });
 
   passport.use(new LocalStrategy({
@@ -57,5 +57,6 @@ passport.deserializeUser(async function (id, done) {
     })
   );
 
+  app.use(passport.initialize());
   app.use(passport.session());
 };
